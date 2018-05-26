c20a1f956b4d230542ac67541d3731decc02216f
Eric Fischer <enf@pobox.com>
2018-05-25 18:10:57 -0700

    Factor out boilerplate

--- ls.cpp
+++ ls.cpp
@@ -80,16 +80,58 @@ void read_flags(std::vector<flag> &flags, const char *fname, bool direct) {
 	json_free(obj);
 }
 
-int main(int argc, char **argv) {
-	av = argv;
+int getopt_json(int argc, char **argv, std::string &getopt_str, struct option *long_options, std::vector<flag> &flags, std::vector<const char *> &to_free) {
+	int i;
 
-	struct option long_options[] = {
-		{"long", no_argument, 0, 'l'},
-		{"recursive", no_argument, 0, 'r'},
-		{"json-options", required_argument, 0, 'j'},
-		{NULL, 0, 0, '\0'},
-	};
+	if (flags.size() > 0) {
+		flag f = flags[0];
+		flags.erase(flags.begin());
 
+		struct option *found = NULL;
+		for (size_t j = 0; long_options[j].name != NULL; j++) {
+			if (f.option == long_options[j].name) {
+				found = &long_options[j];
+				break;
+			}
+
+			if (f.option.size() == 1 && long_options[j].val == f.option[0]) {
+				found = &long_options[j];
+				break;
+			}
+		}
+
+		if (found == NULL) {
+			i = '?';
+			fprintf(stderr, "%s: %s: No such option\n", *av, f.option.c_str());
+		} else {
+			if (found->flag != NULL) {
+				i = 0;
+				*(found->flag) = found->val;
+			} else {
+				i = found->val;
+
+				if (found->has_arg == required_argument && !f.has_arg) {
+					fprintf(stderr, "%s: %s requires an argument but one was not specified\n", *av, found->name);
+					exit(EXIT_FAILURE);
+				}
+
+				if (found->has_arg == no_argument && f.has_arg) {
+					fprintf(stderr, "%s: %s does not require an argument but one was specified\n", *av, found->name);
+					exit(EXIT_FAILURE);
+				}
+
+				optarg = strdup(f.arg.c_str());
+				to_free.push_back(optarg);
+			}
+		}
+	} else {
+		i = getopt_long(argc, argv, getopt_str.c_str(), long_options, NULL);
+	}
+
+	return i;
+}
+
+std::string make_options(struct option *long_options) {
 	std::string getopt_str;
 	for (size_t lo = 0; long_options[lo].name != NULL; lo++) {
 		if (long_options[lo].val > ' ') {
@@ -101,64 +143,28 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	return getopt_str;
+}
+
+int main(int argc, char **argv) {
+	av = argv;
+
+	struct option long_options[] = {
+		{"long", no_argument, 0, 'l'},
+		{"recursive", no_argument, 0, 'r'},
+		{"json-options", required_argument, 0, 'j'},
+		{NULL, 0, 0, '\0'},
+	};
+
 	std::vector<flag> flags;
 	std::vector<const char *> to_free;
+	std::string getopt_str = make_options(long_options);
 
 	bool want_long = false;
 	bool recursive = false;
 
-	while (true) {
-		int i;
-
-		if (flags.size() > 0) {
-			flag f = flags[0];
-			flags.erase(flags.begin());
-
-			struct option *found = NULL;
-			for (size_t j = 0; long_options[j].name != NULL; j++) {
-				if (f.option == long_options[j].name) {
-					found = &long_options[j];
-					break;
-				}
-
-				if (f.option.size() == 1 && long_options[j].val == f.option[0]) {
-					found = &long_options[j];
-					break;
-				}
-			}
-
-			if (found == NULL) {
-				i = '?';
-				fprintf(stderr, "%s: %s: No such option\n", *av, f.option.c_str());
-			} else {
-				if (found->flag != NULL) {
-					i = 0;
-					*(found->flag) = found->val;
-				} else {
-					i = found->val;
-
-					if (found->has_arg == required_argument && !f.has_arg) {
-						fprintf(stderr, "%s: %s requires an argument but one was not specified\n", *av, found->name);
-						exit(EXIT_FAILURE);
-					}
-
-					if (found->has_arg == no_argument && f.has_arg) {
-						fprintf(stderr, "%s: %s does not require an argument but one was specified\n", *av, found->name);
-						exit(EXIT_FAILURE);
-					}
-
-					optarg = strdup(f.arg.c_str());
-					to_free.push_back(optarg);
-				}
-			}
-		} else {
-			i = getopt_long(argc, argv, getopt_str.c_str(), long_options, NULL);
-		}
-
-		if (i == -1) {
-			break;
-		}
-
+	int i;
+	while ((i = getopt_json(argc, argv, getopt_str, long_options, flags, to_free)) != -1) {
 		switch (i) {
 		case 'l':
 			want_long = true;
